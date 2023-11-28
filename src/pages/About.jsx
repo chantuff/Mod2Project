38634bd6fa74d5c5fa59051f08c0744762fbccd0
chantuff <chan.tuff@yahoo.com>
2023-11-28 13:50:48 -0500

    a few small style changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <>
-    <div className='about'>About</div>
+    <div className='about'></div>
     <div className='about-container'>
       <p className='about-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iure rerum earum soluta saepe inventore ut repellat voluptates deleniti autem. Possimus, ex dolores molestias fugiat perspiciatis doloribus quo dolore! Ad.</p>
     </div>
